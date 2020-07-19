470bf6651c443c1a4da044cdca530a535367a10b
JozsefVass <joco.udv@gmail.com>
2020-07-19 14:26:32 +0100

    Concat function

---
+++ lodash/array/concat.js
@@ -0,0 +1,23 @@
+const concat = (array, ...values) => {
+  if (Object.prototype.toString.call(array) !== "[object Array]") {
+    array = [array];
+  }
+
+  const result = [...array];
+  let index = -1;
+
+  while (++index < values.length) {
+    if (Object.prototype.toString.call(values[index]) === "[object Array]") {
+      let length = values[index].length;
+      for (let i = 0; i < length; i++) {
+        result.push(values[index][i]);
+      }
+    } else {
+      result.push(values[index]);
+    }
+  }
+
+  return result;
+};
+
+console.log(concat({ b: "boo" }, 2, [3], [[4]], { a: "foo" }));
